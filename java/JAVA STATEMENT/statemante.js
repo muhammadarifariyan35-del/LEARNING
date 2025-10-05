33e0b91e1f1a458bf4001115504751eecd7e0a56
muhammadarifariyan35-del <muhammadarifariyan35@gmail.com>
2025-10-05 19:31:12 +0600

    new

--- java/JAVA STATEMENT/statemante.js
+++ java/JAVA STATEMENT/statemante.js
@@ -1,6 +1,6 @@
 console.log('hello');
 document.getElementById('title').innerHTML = 'arif ariyan'
-let sum = 50 + 40
+let sum =  50 + 40
 console.log(sum)
 if (sum > 100){
     console.log('expensive')
---
+++ java/variables/variables.js
@@ -0,0 +1,41 @@
+// javascript variables 
+/* 
+
+*/
+
+var arif = 20;
+var arif = "bangladesh"
+arif =  'sharif'
+console.log(arif)
+
+
+{
+var ariyan = 10    
+    
+}
+console.log(ariyan);
+var country = "america"
+console.log(country);
+
+
+let fruits = 'orange';
+console.log(`${fruits} is my favourite, ${fruits} i like to eat`);
+    
+let a = 15;
+a = 16;
+{
+    
+    console.log(a);
+}
+let people = 'some one'
+console.log(people);
+ 
+/* 
+* you can not re declare
+* you can not assaign
+*
+*/
+
+const c = 19
+
+console.log(a);
