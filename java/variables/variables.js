c0607402f1f976f577ee7d8798d84cdb7a687e96
muhammadarifariyan35-del <muhammadarifariyan35@gmail.com>
2025-10-14 19:54:21 +0600

    hello

--- java/variables/variables.js
+++ java/variables/variables.js
@@ -1,8 +1,38 @@
-// javascript variables 
-/* 
 
-*/
 
+//! --------statement -----------------------------------
+/*
+document.getElementById('ctn').innerHTML = 'hello world'
+
+let sum = 90 + 0;
+console.log(sum);
+
+if (sum > 100){
+console.log('Expenseve');
+} else {
+    console.log('cheep');
+}
+
+
+function sayHello(name){
+ console.log('hello ' + name);
+}
+sayHello('arif')
+sayHello('sharif')
+sayHello ('jui')*/
+//?------------------------------------------------------
+
+
+//!-----syntex-------------------------------------------
+
+/*
+let firsName  = 'ali'  //সিনটেক্স মানে হলো ব্যকরন।
+console.log(firsName);*/
+//?------------------------------------------------------
+
+
+//!------variable----------------------------------------
+/*
 var arif = 20;
 var arif = "bangladesh"
 arif =  'sharif'
@@ -14,8 +44,9 @@ var ariyan = 10
     
 }
 console.log(ariyan);
-var country = "america"
+
 console.log(country);
+var country = "america"
 
 
 let fruits = 'orange';
@@ -62,4 +93,156 @@ const hello = {
 }
 hello.jela = 'mymensingh';
 hello.age = 32;
-console.log(hello);
+console.log(hello); */
+
+//?------------------------------------------------------
+
+
+
+//!-------operator---------------------------------------
+
+/*
+//*     -----arithmetic operator-----
+
+
+let a = 55;
+let b = 7;
+//et c = a + b;
+//let c = a - b;
+//let c = a * b;
+//let c = a / b;
+let c = a % b;
+console.log(c);
+
+
+//let userInput = prompt('Enter a number');
+//let number = parseInt(userInput);
+
+//if (number % 2 === 0){
+//console.log(` ${number} number  is even`);
+//} else{
+//    console.log(` ${number} number  is even`);
+//}
+
+//*    ----- increement/ decrement------
+
+let x = 10;
+x = x + 1
+x++;
+++x
+
+console.log(x);
+
+
+let y = 20
+y = y - 1;
+y--;
+--y
+
+console.log(y);
+
+//*    ---------presidence----------
+
+// () {} []
+// / * - + 
+
+console.log(10 + 7 - 9 / 8 * 10);
+
+
+//= 10+7-1.125*10
+//= 10+7-11.25
+//= 10+ -4.25
+//= 5.75
+
+
+//6 / 5 - 3 + 2 * 4 * 4 + 9
+//= 1.2 - 3 + 2 * 4 * 4 + 9
+//= 1.2 - 3 + 8 * 4 + 9
+//= 1.2 - 3 + 32 + 9
+//= -1.8 + 32 + 9 
+//= 30.2 + 9
+//= 39.2
+
+
+console.log(6 / 5 - 3 + 2 * 4 * 4 + 9);
+
+
+
+//* ------comperison operator--------
+
+let a1 = 15;
+let a2 = '10';
+console.log(a1 == a2);
+console.log(a1 === a2);
+console.log(a1 != a2);
+console.log(a1 !== a2);
+console.log(a1 > a2);
+console.log(a1 < a2);
+console.log(a1 >= a2);
+console.log(a1 <= a2);
+
+
+
+
+//* -----logical operator---------
+
+// and
+console.log(true && true);
+console.log(true && false);
+console.log(false && false);
+
+//or
+console.log(true || true);
+console.log(false || true);
+console.log(false || false);
+
+//not
+console.log(!(a1 < a2));
+console.log(!(a2 < a1));
+console.log(!(a2 > a1));
+console.log(!(a1 > a2));
+
+//nor  
+console.log(!(true || true));
+console.log(!(true || false));
+console.log(!(false || true));
+console.log(!(false || false));
+
+
+//*    -----assingment------
+
+let j = 20;
+let k = 10;
+
+j = j + k;
+j +=k;
+
+j = j - k;
+j -=k;
+
+j = j * k;
+j *=k;
+
+j = j / k;
+j /=k;
+
+j = j % k;
+j %=k;
+
+j = j ** k; 
+j **=k
+console.log(j);
+
+
+
+//* ------ternary operator------
+
+let t = a1 < a2 ? 'true' : 'false';
+
+console.log(t);
+console.log(typeof t );
+if (a1 > a2 ) console.log('true'); else console.log('false');
+*/
+
+//?-------------------------------------------------------
+
